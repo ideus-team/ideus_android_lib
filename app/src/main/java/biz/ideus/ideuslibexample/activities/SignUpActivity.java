94cdc2ae72cacb6bb88c83ac93808348d963cae3
dmitry.kravchuk <dmitry.kravchuk@ideus.biz>
2016-11-11 15:48:21 +0200

    some changes

--- app/src/main/java/biz/ideus/ideuslibexample/activities/SignUpActivity.java
+++ app/src/main/java/biz/ideus/ideuslibexample/activities/SignUpActivity.java
@@ -1,8 +1,23 @@
 package biz.ideus.ideuslibexample.activities;
 
+import android.view.View;
+
+import biz.ideus.ideuslib.activity.DLibBindingActivity;
+import biz.ideus.ideuslibexample.R;
+import biz.ideus.ideuslibexample.databinding.SignUpActivityBinding;
+
 /**
  * Created by blackmamba on 11.11.16.
  */
 
-public class SignUpActivity {
+public class SignUpActivity extends DLibBindingActivity<SignUpActivityBinding> {
+    @Override
+    public int getLayoutId() {
+        return R.layout.sign_up_activity;
+    }
+
+    @Override
+    public void onInit(View rootView) {
+
+    }
 }
