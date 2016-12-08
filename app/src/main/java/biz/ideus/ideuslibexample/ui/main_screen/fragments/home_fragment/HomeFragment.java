ce45d30c099c17fb6d8e9f4ee580350f61ba9a5b
alexander.zhuk <alexander.zhuk@ideus.biz>
2016-12-08 15:38:39 +0200

    1

--- app/src/main/java/biz/ideus/ideuslibexample/ui/main_screen/fragments/home_fragment/HomeFragment.java
+++ app/src/main/java/biz/ideus/ideuslibexample/ui/main_screen/fragments/home_fragment/HomeFragment.java
@@ -1,14 +1,15 @@
 package biz.ideus.ideuslibexample.ui.main_screen.fragments.home_fragment;
 
-import android.content.Context;
+import android.animation.Animator;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.animation.FastOutLinearInInterpolator;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
+import android.widget.LinearLayout;
 
 import biz.ideus.ideuslib.mvvm_lifecycle.binding.ViewModelBindingConfig;
-import biz.ideus.ideuslibexample.BR;
 import biz.ideus.ideuslibexample.R;
+import biz.ideus.ideuslibexample.BR;
 import biz.ideus.ideuslibexample.databinding.FragmentHomeBinding;
 import biz.ideus.ideuslibexample.ui.base.BaseFragment;
 import biz.ideus.ideuslibexample.ui.start_screen.StartView;
@@ -20,7 +21,8 @@ import biz.ideus.ideuslibexample.ui.start_screen.StartView;
 public class HomeFragment extends BaseFragment<StartView, HomeFragmentVM, FragmentHomeBinding>
 implements StartView {
 
-
+    LinearLayout mRevealView ;
+    LinearLayout myView ;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -32,12 +34,56 @@ implements StartView {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         setModelView(this);
+
+        myView = getBinding().text;
+       // mRevealView = getBinding().text;
+
+        getBinding().btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // get the center for the clipping circle
+                int cx = (myView.getLeft() + myView.getRight()) / 2;
+                int cy = (myView.getTop() + myView.getBottom()) / 2;
+
+                // get the final radius for the clipping circle
+                int dx = Math.max(cx, myView.getWidth() - cx);
+                int dy = Math.max(cy, myView.getHeight() - cy);
+                float finalRadius = (float) Math.hypot(dx, dy);
+
+                float endRadius = (float) Math.hypot(
+                        Math.max(cx, myView.getWidth() - cx),
+                        Math.max(cy, myView.getHeight() - cy));
+
+                // Android native animator
+                Animator animator = io.codetail.animation.ViewAnimationUtils.createCircularReveal(myView, cx, cy, 10, finalRadius, View.LAYER_TYPE_NONE);
+
+
+               // Animator animator = ViewAnimationUtils.createCircularReveal(myView, cx, cy, 0, finalRadius);
+                animator.setInterpolator(new FastOutLinearInInterpolator());
+                animator.setDuration(1600);
+                animator.start();
+            }
+        });
+
+//        getBinding().btn.setOnClickListener(view1 -> {
+//            int cx = (mRevealView.getRight() + mRevealView.getLeft()) / 2;
+//            int cy = mRevealView.getTop();
+//            int endradius = Math.max(mRevealView.getWidth(), mRevealView.getHeight());
+//
+//            Animator mAnimator =
+//                    ViewAnimationUtils.createCircularReveal(mRevealView, cx, cy, 0, endradius);
+//           // mAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+//            mAnimator.setInterpolator(new FastOutLinearInInterpolator());
+//            mAnimator.setDuration(1600);
+//            mAnimator.start();
+//
+//        });
     }
 
-    public void showKeyBoard(){
-        InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
-        imm.showSoftInput(getBinding().itemSearch.etSearch, InputMethodManager.SHOW_IMPLICIT);
-    }
+//    public void showKeyBoard(){
+//        InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+//        imm.showSoftInput(getBinding().itemSearch.etSearch, InputMethodManager.SHOW_IMPLICIT);
+//    }
 
     @Nullable
     @Override
