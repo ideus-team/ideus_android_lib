dee00021810946b6a2474680998b0f8503bf4ed6
alexander.zhuk <alexander.zhuk@ideus.biz>
2017-02-13 15:43:04 +0200

    1

---
+++ app/src/boarderFlavor/java/biz/ideus/ideuslibexample/ui/board_details/activity/BoardDetailsActivity.java
@@ -0,0 +1,8 @@
+package biz.ideus.ideuslibexample.ui.board_details.activity;
+
+/**
+ * Created by user on 08.02.2017.
+ */
+
+public class BoardDetailsActivity {
+}
---
+++ app/src/boarderFlavor/java/biz/ideus/ideuslibexample/ui/board_details/activity/BoardDetailsVM.java
@@ -0,0 +1,8 @@
+package biz.ideus.ideuslibexample.ui.board_details.activity;
+
+/**
+ * Created by user on 09.02.2017.
+ */
+
+public class BoardDetailsVM {
+}
