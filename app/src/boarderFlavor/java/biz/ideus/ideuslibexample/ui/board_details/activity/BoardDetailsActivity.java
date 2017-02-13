353761cc8371de2473c7689526461b0a236b1b24
alexander.zhuk <alexander.zhuk@ideus.biz>
2017-02-13 15:43:09 +0200

    1

--- app/src/boarderFlavor/java/biz/ideus/ideuslibexample/ui/board_details/activity/BoardDetailsActivity.java
+++ app/src/boarderFlavor/java/biz/ideus/ideuslibexample/ui/board_details/activity/BoardDetailsActivity.java
@@ -1,8 +1,28 @@
 package biz.ideus.ideuslibexample.ui.board_details.activity;
 
+import android.support.annotation.Nullable;
+
+import biz.ideus.ideuslib.mvvm_lifecycle.binding.ViewModelBindingConfig;
+import biz.ideus.ideuslibexample.databinding.ActivityMainBinding;
+import biz.ideus.ideuslibexample.ui.base.BaseActivity;
+import biz.ideus.ideuslibexample.ui.main_screen.activity.MainActivityVM;
+import biz.ideus.ideuslibexample.ui.start_screen.StartView;
+
 /**
  * Created by user on 08.02.2017.
  */
 
-public class BoardDetailsActivity {
+public class BoardDetailsActivity  extends BaseActivity<StartView, MainActivityVM, ActivityMainBinding>
+        implements StartView {
+    @Nullable
+    @Override
+    public Class<MainActivityVM> getViewModelClass() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public ViewModelBindingConfig getViewModelBindingConfig() {
+        return null;
+    }
 }
